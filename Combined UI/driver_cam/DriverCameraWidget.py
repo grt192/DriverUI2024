34254939174fe2c1ec85ac7d8f397991a468a4a2
Pointy Ice <jessecsh7@gmail.com>
2024-01-09 17:45:56 -0800

    Implemented Video Stream

--- Combined UI/driver_cam/DriverCameraWidget.py
+++ Combined UI/driver_cam/DriverCameraWidget.py
@@ -7,7 +7,9 @@ from PySide6.QtGui import *
 from pyqtgraph import ImageItem, PlotWidget
 import numpy as np
 from time import perf_counter
-
+import cv2
+import requests
+from threading import *
 class CameraWidget(QWidget):
     def __init__(self, parent=None):
         super(CameraWidget, self).__init__(parent)
@@ -35,7 +37,10 @@ class CameraWidget(QWidget):
 
         self.timer = QtCore.QTimer()
         self.timer.timeout.connect(self.update_frame)
-        self.timer.start(1)  # Adjust the interval as needed (e.g., 100 ms for 10 FPS)
+        #self.timer.start(1)  # Adjust the interval as needed (e.g., 100 ms for 10 FPS)
+
+        CamThread = Thread(target=self.DisplayStream)
+        CamThread.start()
 
         self.actual_fps = 0
         self.past_five_instantaneous_fps = [0, 0, 0, 0, 0]
@@ -77,6 +82,33 @@ class CameraWidget(QWidget):
         total = sum(self.past_five_instantaneous_fps)
         avg = total / 5
         return avg
+
+    def DisplayStream(self):
+        url = "http://10.1.92.2:1181/stream.mjpg"
+        response = requests.get(url, stream=True)
+        bytes = b''
+        for chunk in response.iter_content(chunk_size=1024):
+            if len(bytes) >  1024*512:
+                bytes = b''
+            bytes += chunk
+            a = bytes.find(b'\xff\xd8')  # JPEG start
+            b = bytes.find(b'\xff\xd9')  # JPEG end
+            if a != -1 and b != -1:
+                jpg = bytes[a:b + 2]  # Extract the JPEG image
+                bytes = bytes[b + 2:]  # Remove the processed bytes
+
+                # Decode the JPEG image to a frame
+                frame = cv2.imdecode(np.frombuffer(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)
+
+                # Convert to QImage
+                height, width, channel = frame.shape
+                bytesPerLine = 3 * width
+                qImg = QImage(frame.data, width, height, bytesPerLine, QImage.Format_RGB888).rgbSwapped()
+                pixmap = QPixmap.fromImage(qImg)
+                scaledPixmap = pixmap.scaled(300,300)
+
+
+                self.camera_display.setPixmap(scaledPixmap)
 
 
 class DriverCameraWindow(QMainWindow):
