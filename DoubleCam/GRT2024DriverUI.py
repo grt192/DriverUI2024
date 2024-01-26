709240c13a1d3700fd91e6b7c338a667468b53f9
Pointy Ice <jessecsh7@gmail.com>
2024-01-25 22:17:59 -0800

    Added Cam Widget and sendDestination Widget

--- DoubleCam/GRT2024DriverUI.py
+++ DoubleCam/GRT2024DriverUI.py
@@ -1,5 +1,5 @@
 import sys
-#PySide6 imports
+# PySide6 imports
 from PySide6 import QtCore, QtGui, QtWidgets
 from PySide6.QtCore import *
 from PySide6.QtGui import *
@@ -7,13 +7,17 @@ from PySide6.QtWidgets import *
 
 from Helpers.NetworktableHelper import NetworkTableManager
 from Widgets.ControlWidget import ControlWidget
+from Widgets.MapWidget import MapWidget
+from Widgets.DriverCameraWidget import CameraWidget
+from Widgets.ToggleWidget import ToggleWidget
+
 
 class GRT2024DriverUI(QMainWindow):
     def __init__(self):
         super().__init__()
 
         self.setWindowTitle("GRT 2024 Driver UI")
-        self.resize(1920,1080)
+        self.resize(1920, 1080)
 
         self.mainLayout = QHBoxLayout()
 
@@ -22,7 +26,32 @@ class GRT2024DriverUI(QMainWindow):
         self.centralWidget.setLayout(self.mainLayout)
 
         self.controlWidget = ControlWidget()
+        self.controlWidget.setMaximumWidth(200)
         self.mainLayout.addWidget(self.controlWidget)
+
+        self.mapWidget = MapWidget(
+            self.controlWidget.allianceToggle.currentText
+        )
+        self.controlWidget.allianceToggle.toggled.connect(
+            self.mapWidget.changeAllianceColor
+        )
+        self.mainLayout.addWidget(self.mapWidget)
+
+        self.sendDestinationWidget = ToggleWidget(
+            "Send Destination", "", "", ("Send", "Sending...")
+        )
+        self.sendDestinationWidget.setMaximumWidth(100)
+        self.mainLayout.addWidget(self.sendDestinationWidget)
+
+        self.cameraLayout = QVBoxLayout()
+        self.mainLayout.addLayout(self.cameraLayout)
+
+        self.cameraWidget1 = CameraWidget()
+        self.cameraWidget2 = CameraWidget()
+        self.cameraLayout.addWidget(self.cameraWidget1)
+        self.cameraLayout.addWidget(self.cameraWidget2)
+
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = GRT2024DriverUI()
---
+++ DoubleCam/Widgets/MapWidget.py
@@ -0,0 +1,63 @@
+import os
+from PySide6.QtWidgets import QWidget, QLabel, QSizePolicy, QVBoxLayout
+from PySide6.QtGui import QPixmap, QTransform
+from PySide6.QtCore import Qt
+import sys
+from PySide6.QtWidgets import QApplication
+    
+class MapWidget(QWidget):
+    def __init__(self, alliance, parent=None):
+        super().__init__(parent)
+        
+        self.mapLabel = QLabel()
+        self.mapLabel.setScaledContents(True)
+        
+        self.mapPixmap = QPixmap("./images/field24.png").scaled(600, 300)
+        self.robotPixmap = QPixmap("./images/grt23robot.png")
+        self.crosshairPixmap = QPixmap("./images/crosshair.png")
+        
+        self.alliance = alliance
+
+        if self.alliance == "red":
+            rotationAngle = 90
+        else:
+            rotationAngle = -90
+        transform = QTransform().rotate(rotationAngle)
+        self.mapPixmap = self.mapPixmap.transformed(transform)
+        
+        # Set up QLabel for robot icon
+        self.robotLabel = QLabel(self)
+        self.robotLabel.setPixmap(self.robotPixmap)
+        self.robotLabel.setMask(self.robotPixmap.mask())  # Use transparency information for masking
+        
+        # Set up QLabel for crosshair (position it at the center for now)
+        self.crosshairLabel = QLabel(self)
+        self.crosshairLabel.setGeometry(self.width() // 2 - self.crosshairPixmap.width() // 2,
+                                        self.height() // 2 - self.crosshairPixmap.height() // 2,
+                                        self.crosshairPixmap.width(), self.crosshairPixmap.height())
+        self.crosshairLabel.setPixmap(self.crosshairPixmap)
+        # self.crosshairLabel.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        
+        self.mapLabel.setPixmap(self.mapPixmap)
+        layout = QVBoxLayout()
+        layout.addWidget(self.mapLabel)
+        self.setLayout(layout)
+        
+    def changeAllianceColor(self, new_alliance_color):
+        self.alliance = new_alliance_color
+        self.reloadMaps()
+
+    def reloadMaps(self):
+        self.mapPixmap = QPixmap("./images/field24.png").scaled(600, 300)
+        rotation_angle = 90 if self.alliance == "red" else -90
+        transform = QTransform().rotate(rotation_angle)
+        self.mapPixmap = self.mapPixmap.transformed(transform)
+        self.mapLabel.setPixmap(self.mapPixmap)
+        
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    mw = MapWidget('red')
+    
+    mw.show()
+    sys.exit(app.exec())
