02ddda7d378cf8f4de77b8dc1ab6423504ea4d3b
Pointy Ice <jessecsh7@gmail.com>
2024-03-09 17:01:33 -0800

    Fixed dimentions and makes robotLabel a member of mapLabel. Everything works perfect!

--- Widgets/MapDisplayWidget.py
+++ Widgets/MapDisplayWidget.py
@@ -1,30 +1,32 @@
 from PySide6.QtWidgets import QWidget, QLabel, QSizePolicy, QVBoxLayout
 from PySide6.QtGui import QPixmap, QTransform
-from PySide6.QtCore import Signal
-import time
-import sys
+from PySide6.QtCore import Signal, QTimer
 from PySide6.QtWidgets import QApplication
+import sys
+import os
 from Helpers.NestedNetworktableHelper import NestedNetworkTableManager
 
 
 class MapDisplayWidget(QWidget):
     newCrosshairPosition = Signal(tuple)
-    fieldX = 16.591788
-    fieldY = 8.211312
+    fieldX = 16.451
+    fieldY = 8.211
+    mapX = 400
+    mapY = 810
+    robotScale = 50
 
     def __init__(self, alliance, parent=None):
         super().__init__(parent)
-        self.setContentsMargins(0,0,0,0)
 
         self.mapLabel = QLabel()
         self.mapLabel.setScaledContents(True)
-        self.mapLabel.setContentsMargins(0,0,0,0)
+        self.mapLabel.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        self.mapLabel.setMaximumWidth(self.mapX)
+        self.mapLabel.setMaximumHeight(self.mapY)
 
         self.mapPixmap = QPixmap("./Images/field24.png")
-        self.robotScale = 40
         self.robotPixmap = QPixmap("./Images/robot_frame.png").scaled(self.robotScale,
                                                                     self.robotScale)
-        self.crosshairPixmap = QPixmap("./Images/crosshair.png")
 
         self.alliance = alliance
 
@@ -36,20 +38,11 @@ class MapDisplayWidget(QWidget):
         self.mapPixmap = self.mapPixmap.transformed(transform)
 
         # Set up QLabel for robot icon
-        self.robotLabel = QLabel(self)
-        self.robotLabel.setContentsMargins(0,0,0,0)
+        self.robotLabel = QLabel(self.mapLabel)
         self.robotLabel.setPixmap(self.robotPixmap)
         self.robotLabel.setSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
         self.robotLabel.setMask(self.robotPixmap.mask())  # Use transparency information for masking
 
-        # Set up QLabel for crosshair (hide it for now)
-        self.crosshairLabel = QLabel(self)
-        self.crosshairLabel.setPixmap(self.crosshairPixmap)
-        self.crosshairLabel.hide()
-        self.crosshairLabel.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
-            # init crosshair position
-        self.crosshairX = None
-        self.crosshairY = None
 
         self.robotPoseRootNTTableName = "Shuffleboard"
         self.robotPoseNTTableNames = ["Auton", "Field"]
@@ -65,6 +58,14 @@ class MapDisplayWidget(QWidget):
         layout.addWidget(self.mapLabel)
         self.setLayout(layout)
 
+
+        #Debug:
+        # self.timer = QTimer()
+        # self.timer.timeout.connect(self.printSize)
+        # self.timer.start(1000)
+
+        # self.robotLabel.setGeometry(0, 0, self.robotScale, self.robotScale)
+
     def changeAllianceColor(self, new_alliance_color):
         self.alliance = new_alliance_color
         self.reloadMaps()
@@ -76,33 +77,13 @@ class MapDisplayWidget(QWidget):
         self.mapPixmap = self.mapPixmap.transformed(transform)
         self.mapLabel.setPixmap(self.mapPixmap)
 
-    def mousePressEvent(self, event):
-        # Get the position of the mouse click
-        position = event.pos()
-        print(f"Clicked at position: {position.x()}, {position.y()}")
-        self.crosshairLabel.hide()
-        self.crosshairLabel.setGeometry(position.x() - 12, position.y() - 12, 25, 25)
-        self.crosshairX = position.x()
-        self.crosshairY = position.y()
-        self.crosshairLabel.show()
-        self.crosshairLabel.raise_()
-        self.newCrosshairPosition.emit((self.crosshairX, self.crosshairY))
-
     def updateRobotPose(self, entryName, entryValue):
-        print("height" + str(self.height()))
-        print()
         self.robotLabel.hide()
-        #type is tuple
-        # return
-        # print(entryValue)
-        # print(range(len(entryValue)))
         for i in range(len(entryValue)):
             self.robotPose[i] = entryValue[i]
-        # print(self.robotPose)
-        newRobotPose = [self.robotPose[0] / self.fieldX * self.height(),
-                        self.robotPose[1] / self.fieldY * self.width(),
+        newRobotPose = [self.robotPose[0] / self.fieldX * self.mapY,
+                        self.robotPose[1] / self.fieldY * self.mapX,
                         self.robotPose[2]]
-        #print(newRobotPose)
         if self.alliance == "blue":
             self.robotLabel.setPixmap(
                 self.robotPixmap.scaled(
@@ -113,11 +94,14 @@ class MapDisplayWidget(QWidget):
                 )
             )
             self.robotLabel.setGeometry(
-                int(self.mapLabel.width() - newRobotPose[1] - self.robotScale / 2 ),
-                int(self.mapLabel.height() - newRobotPose[0] - self.robotScale / 2),
+                int(self.mapX - newRobotPose[1] - self.robotScale / 2),
+                int(self.mapY - newRobotPose[0] - self.robotScale / 2),
                 self.robotScale,
                 self.robotScale
             )
+            # print(int(self.mapX - newRobotPose[1] - self.robotScale / 2))
+            # print(int(self.mapY - newRobotPose[0] - self.robotScale / 2))
+            # print("-----------------------")
         else:
             self.robotLabel.setPixmap(
                 self.robotPixmap.scaled(
@@ -136,8 +120,15 @@ class MapDisplayWidget(QWidget):
         self.robotLabel.show()
         self.robotLabel.raise_()
         self.robotLabel.raise_()
-        #print(self.mapLabel.height())
 
+    def printSize(self):
+        print("Entire widget size:")
+        print(self.size())
+        print("Map label size:")
+        print(self.mapLabel.size())
+        print("Robot label size:")
+        print(self.robotLabel.size())
+        print("---------------------")
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
