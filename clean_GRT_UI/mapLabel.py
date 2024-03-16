a5a8e3a9178360ce26d680e38e6e4d4c3f965649
Pointy Ice <jessecsh7@gmail.com>
2024-03-15 18:23:17 -0700

    deleted old version

--- clean_GRT_UI/mapLabel.py
+++
@@ -1,8 +0,0 @@
-from PySide6.QtWidgets import QLabel
-
-class mapLabel(QLabel):
-    def __init__(self, widget, delegate):
-        super().__init__(widget)
-        self.delegate = delegate
-    def mousePressEvent(self, event):
-       self.delegate.getClickPosition(event.pos())
