95648ffdde4ce896045cfaca933355ddea3525f4
Jesse Chu <jesse.chu@outlook.com>
2024-03-30 23:07:51 -0700

    Removed unnecessary methods.

--- Helpers/NetworktableManager.py
+++ Helpers/NetworktableManager.py
@@ -18,26 +18,3 @@ class NetworkTableManager(QObject):
         if key == self.entry_name:
             self.new_value_available.emit((key, value))
 
-    def putString(self, message: str):
-        print("puting: " + message)
-        if (type(message) is not str):
-            raise Exception("Wrong message type!")
-        self.table.putString(self.entry_name, str(message))
-        #cnt is just for debug
-        self.cnt += 1
-
-    def putBool(self, message: bool):
-        print("puting: " + str(message) + "\nCurrent table: " + self.tableName + " entry: " + self.entry_name)
-        if(type(message) is not bool):
-            raise Exception("Wrong message type!")
-        self.table.putBoolean(self.entry_name, message)
-        print("finished")
-    def putInt(self, message: int):
-        if(type(message) is not int):
-            raise Exception("Wrong message type!")
-        self.table.putNumber(self.entry_name, message)
-
-
-        
-        
-        
