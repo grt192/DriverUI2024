195cd35182930dd8c901adb4ee39b54c7367f615
Jesse Chu <jesse.chu@outlook.com>
2024-03-30 22:52:15 -0700

    Renamed tableName and entryName.

--- Helpers/NetworktableManager.py
+++ Helpers/NetworktableManager.py
@@ -3,22 +3,18 @@ from PySide6.QtCore import Signal, QObject
 import requests
 class NetworkTableManager(QObject):
     new_value_available = Signal(tuple)
-
-    #nt debug
-    cnt = 0
-    def __init__(self, table_name, entry_name="") -> None:
-        super().__init__()
+    def __init__(self, tableName, entryName, parent = None):
+        super().__init__(parent)
         NetworkTables.initialize(server='10.1.92.2')
 
         #print("creating table: "+ table_name)
-        self.tableName = table_name
-        self.table = NetworkTables.getTable(table_name)
-        self.entry_name = entry_name
+        self.tableName = tableName
+        self.table = NetworkTables.getTable(tableName)
+        self.entry_name = entryName
         
         self.table.addEntryListener(self.valueChanged)
 
     def valueChanged(self, table, key, value, isNew):
-        # print("Key: " + key + " Value: " + str(value))
         if key == self.entry_name:
             self.new_value_available.emit((key, value))
 
