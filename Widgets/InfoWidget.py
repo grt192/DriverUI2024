153117f90408baf27325f3b94061c53c8d89ae42
Jesse Chu <jesse.chu@outlook.com>
2024-04-03 13:38:19 -0700

    Moved some entries to the second InfoWidget

--- Widgets/InfoWidget.py
+++ Widgets/InfoWidget.py
@@ -1,8 +1,6 @@
 from PySide6.QtCore import Signal, QTimer
 from PySide6.QtWidgets import QWidget, QVBoxLayout, QSizePolicy, QPushButton
 from Widgets.CustomWidgets.BaseWidgets.BoolDisplayLabel import BoolDisplayLabel
-from Widgets.CustomWidgets.BaseWidgets.IntDisplayLabel import IntDisplayLabel
-from Widgets.CustomWidgets.BaseWidgets.DoubleDisplayLabel import DoubleDisplayLabel
 from Widgets.CustomWidgets.BaseWidgets.ConnectionDisplayLabel import ConnectionDisplayLabel
 from Widgets.CustomWidgets.AllianceLabel import AllianceLabel
 from Widgets.CustomWidgets.BaseWidgets.GradientDoubleDisplayLabel import GradientDoubleDisplayLabel
@@ -15,12 +13,12 @@ class InfoWidget(QWidget):
         self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
 
         self.timer = QTimer()
-        self.timer.start(500)
+        self.timer.start(100)
         self.allianceLabel = AllianceLabel(self)
         self.timeLeftLabel = GradientWarningDoubleDisplayLabel(
             "Time", "FMS", "TimeLeft",
             (255, 105, 0), (255, 219, 194),
-            20., 135,
+            10., 135,
             self.timer,
             parent = self, debug = False
         )
@@ -36,61 +34,7 @@ class InfoWidget(QWidget):
             (1, 188, 70),(254, 66, 30),
             parent = self, debug = True
         )
-        self.motor10CurrentLabel = GradientWarningDoubleDisplayLabel(
-            "10 Current", "Motors", "10Current",
-            (0,180, 70), (1, 181, 71),
-            0, 40,
-            self.timer,
-            "color: white; font-weight: bold; font-size: 20px;",
-            parent = self, debug = False
-        )
-        self.motor10VoltageLabel = GradientWarningDoubleDisplayLabel(
-            "10 Voltage: ", "Motors", "10Voltage",
-            (0, 180, 70), (1, 181, 71),
-            12, 13,
-            self.timer,
-            "color: white; font-weight: bold; font-size: 20px;",
-            parent = self, debug = False
-        )
 
-        self.motor10TemperatureLabel = GradientWarningDoubleDisplayLabel(
-            "10 Temp", "Motors", "10Temperature",
-            (153, 215, 189), (243, 95, 0),
-            10, 50,
-            self.timer,
-            "color: white; font-weight: bold; font-size: 20px;",
-            parent = self, debug = False
-        )
-        self.motor11CurrentLabel = GradientWarningDoubleDisplayLabel(
-            "11 Current", "Motors", "11Current",
-            (0, 180, 70), (1, 181, 71),
-            0, 40,
-            self.timer,
-            "color: white; font-weight: bold; font-size: 20px;",
-            parent=self, debug=False
-        )
-        self.motor11VoltageLabel = GradientWarningDoubleDisplayLabel(
-            "11 Voltage: ", "Motors", "11Voltage",
-            (0, 180, 70), (1, 181, 71),
-            12, 13,
-            self.timer,
-            "color: white; font-weight: bold; font-size: 20px;",
-            parent=self, debug=False
-        )
-        self.motor11TemperatureLabel = GradientWarningDoubleDisplayLabel(
-            "11 Temp", "Motors", "11Temperature",
-            (153, 215, 189), (243, 95, 0),
-            10, 50,
-            self.timer,
-            "color: white; font-weight: bold; font-size: 20px;",
-            parent=self, debug= False
-        )
-        self.visionConnectionLabel = ConnectionDisplayLabel(
-            "Vision", "http://10.1.92.13:5800",
-            (0,180,70), (255, 0, 0),
-            "color: white; font-weight: bold; font-size: 16px;",
-            parent = self
-        )
         self.ampSensorLabel = BoolDisplayLabel(
             "AMP Sensor", "Intake", "AMPSensor",
             (0, 255, 0), (255, 0, 0),
@@ -98,13 +42,19 @@ class InfoWidget(QWidget):
         )
         self.frontSensorLabel = BoolDisplayLabel(
             "Front Sensor", "Intake", "FrontSensor",
-            (0,255,0), (255, 0, 0),
-            parent = self
+            (0, 255, 0), (255, 0, 0),
+            parent=self
         )
         self.rockwellSensorLabel = BoolDisplayLabel(
             "R Sensor", "Intake", "Rockwell",
             (0, 255, 0), (255, 0, 0),
             parent=self
+        )
+        self.visionConnectionLabel = ConnectionDisplayLabel(
+            "Vision", "http://10.1.92.13:5800",
+            (0,180,70), (255, 0, 0),
+            "color: white; font-weight: bold; font-size: 16px;",
+            parent = self
         )
         self.updateButton = QPushButton("Update")
         self.updateButton.setStyleSheet("color: white; font-weight: bold; font-size: 20px;")
@@ -115,16 +65,11 @@ class InfoWidget(QWidget):
         layout.addWidget(self.timeLeftLabel)
         layout.addWidget(self.elevatorExtensionPercentLabel)
         layout.addWidget(self.elevatorLimitSwitchLabel)
-        layout.addWidget(self.motor10CurrentLabel)
-        layout.addWidget(self.motor10VoltageLabel)
-        layout.addWidget(self.motor10TemperatureLabel)
-        layout.addWidget(self.motor11CurrentLabel)
-        layout.addWidget(self.motor11VoltageLabel)
-        layout.addWidget(self.motor11TemperatureLabel)
-        layout.addWidget(self.visionConnectionLabel)
+
         layout.addWidget(self.ampSensorLabel)
         layout.addWidget(self.frontSensorLabel)
         layout.addWidget(self.rockwellSensorLabel)
+        layout.addWidget(self.visionConnectionLabel)
         layout.addWidget(self.updateButton)
 
     def updateLabels(self):
@@ -132,10 +77,8 @@ class InfoWidget(QWidget):
         self.timeLeftLabel.manualUpdate()
         self.elevatorExtensionPercentLabel.manualUpdate()
         self.elevatorLimitSwitchLabel.manualUpdate()
-        self.motor10CurrentLabel.manualUpdate()
-        self.motor10VoltageLabel.manualUpdate()
-        self.motor10TemperatureLabel.manualUpdate()
-        self.motor11CurrentLabel.manualUpdate()
-        self.motor11VoltageLabel.manualUpdate()
-        self.motor11TemperatureLabel.manualUpdate()
+        self.frontSensorLabel.update()
+        self.ampSensorLabel.update()
+        self.rockwellSensorLabel.update()
         self.visionConnectionLabel.updateConnectionStatus()
+
