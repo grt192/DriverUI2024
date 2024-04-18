304325c3914e6f4ef6b40a54a8025db6994c0efb
Jesse Chu <jesse.chu@outlook.com>
2024-04-17 19:27:52 -0700

    Removes the default update to A when getting available auton lists from robot.

--- Widgets/CustomWidgets/BaseWidgets/AutonSelectionLabel.py
+++ Widgets/CustomWidgets/BaseWidgets/AutonSelectionLabel.py
@@ -6,9 +6,7 @@ class AutonSelectionLabel(QLabel):
         super().__init__(parent)
         self.selectionNTManager = NetworkTableManager("Auton", "Auton")
         self.listNTManager = NetworkTableManager("Auton", "AutonList")
-        self.listNTManager.new_value_available.connect(self.updateList)
         self.combo = QComboBox()
-        self.combo.currentIndexChanged.connect(self.update)
         self.combo.setStyleSheet("color: white; font-weight: bold; font-size: 12px; background-color: black")
         # print(list(self.listNTManager.getValue()).sort())
         sortedList = list(self.listNTManager.getValue())
@@ -17,8 +15,14 @@ class AutonSelectionLabel(QLabel):
         self.layout = QVBoxLayout(self)
         self.layout.addWidget(self.combo)
         self.setLayout(self.layout)
+
+        self.listNTManager.new_value_available.connect(self.updateList)
+        self.combo.currentIndexChanged.connect(self.update)
+
     def update(self):
         self.selectionNTManager.putString(self.combo.currentText())
+        print("Auton selector updates")
     def updateList(self, message: tuple):
         self.combo.clear()
         self.combo.addItems(message[1])
+        print("Auton selector updatesList")
